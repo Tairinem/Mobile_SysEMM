c0e692cd8be5fb1a78a11828ca3f791d119b3b0f
Tairine <t.mousinho.de.lima@avanade.com>
2019-07-05 23:59:03 -0300

    inclusao de página de login

--- SysEMM/index.js
+++ SysEMM/index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Login from './android/app/src/Login/login'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
