16d3125ce150146de2cd65c2d649b0afe02c75c5
Tairine <t.mousinho.de.lima@avanade.com>
2019-07-08 23:25:23 -0300

    Customização da tela de login

--- SysEMM/android/app/src/Login/login.js
+++ SysEMM/android/app/src/Login/login.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Input } from 'react-native-elements';
+import { yellow } from 'ansi-colors';
 
-import { View, Text } from 'react-native';
 
 // import { Container } from './styles';
 
 export default class Login extends Component {
+  state = {
+    title: 'Meu app',
+    description: 'Uma descrição'
+  };
   render() {
     return (
-        <View>
-        <Text>Step One</Text>
-        </View>
+      <View style={styles.container}>
+        <Input style={styles.body}
+          placeholder='Login'
+          errorStyle={{ color: 'red' }}
+        // errorMessage='ENTER A VALID ERROR HERE'
+        />
+        <Input
+          placeholder='Senha'
+          errorStyle={{ color: 'red' }}
+        // errorMessage='ENTER A VALID ERROR HERE'
+        />
+      </View>
     )
   }
-}
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+});
